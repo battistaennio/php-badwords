2f38dff2268c604ba1c37084474a4829a3d0e37d
battistaennio <ennio.battista99@gmail.com>
2024-07-23 15:31:58 +0200

    feat: print p and its length

--- redirect.php
+++ redirect.php
@@ -1,6 +1,8 @@
 <?php
 
+$text = $_POST["paragrafo"];
 
+$censored = $_POST["parola"];
 
 ?>
 
@@ -10,9 +12,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Risultato</title>
 </head>
+
 <body>
-    
+    <!-- stampo paragrafo e la sua lunghezza -->
+    <h2>Il testo inserito contiene <?php echo strlen($text) ?> caratteri, ed è:</h2>
+    <p><?php echo $text ?></p>
 </body>
+
 </html>
